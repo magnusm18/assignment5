d9566959cdf081db526bb9bcb13e238ac210b440
magnusm18 <magnusm18@ru.is>
2019-09-05 15:37:37 +0000

    sequence3

--- sequence.py
+++ sequence.py
@@ -2,9 +2,12 @@ n = int(input("Enter the length of the sequence: "))
 n1 = 1
 n2 = 2
 n3 = 3
-for i in range(1,n+1):
-    heild = n1 + n2 + n3
+print(n1)
+print(n2)
+print(n3)
+for i in range(1,n-2):
+    heild = n1+n2+n3
+    n1 = n2
+    n2 = n3
+    n3 = heild
     print(heild)
-    heild = n3
-    n2 = n1
-    n1 = n1
