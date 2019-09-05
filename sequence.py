9f705637d34200251071af10c8adc278ab5f540d
magnusm18 <magnusm18@ru.is>
2019-09-05 14:59:32 +0000

    sequence algorithm

--- sequence.py
+++ sequence.py
@@ -1,1 +1,8 @@
-n = int(input("Enter the length of the sequence: ")) # Do not change this line
+n = int(input("Enter the length of the sequence: "))
+n1 = 0
+n2 = 0
+n3 = 0
+heild = n1 + n2 + n3
+for i in range(1,n+1):
+
+    
